2593a3fcf434a837fe7504fb0c3073d572f1a390
Sanket vadchhak <128171370+SanketVadchhak@users.noreply.github.com>
2025-09-19 13:35:39 +0530

    Add files via upload

---
+++ chapter_02/methods.py
@@ -0,0 +1,8 @@
+favorite_language = ' python '
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
+
+print("")
+nostarch_url = 'https://nostarch.com'
+print(nostarch_url.removeprefix('https://').removesuffix('.com'))
